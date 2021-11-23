c73cda906371532061a711a8c0ef9a5a1e11332d
Daria Zherebtsova <dazhe2309@yandex.ru>
2021-11-23 18:04:20 +0300

    add LineChart

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,65 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      Индексы потребительских цен на товары и услуги
+    </v-app-bar>
+
+    <v-main class="chart-wrapper">
+      <LineChart :chartdata="chartdata" :options="options" />
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LineChart from './components/LineChart';
 
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
-  }
-}
+    LineChart,
+  },
+
+  data: () => ({
+    chartdata: {
+        labels: [
+          "January",
+          "February",
+          "March",
+          "April",
+          "May",
+          "June",
+          "July"
+        ],
+        datasets: [
+          {
+            label: "Data 1",
+            data: [2, 10, 5, 9, 0, 6, 15],
+            backgroundColor: "transparent",
+            borderColor: "rgba(1, 116, 188, 0.50)",
+            pointBackgroundColor: "rgba(171, 71, 188, 1)"
+          }
+        ]
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        title: {
+          display: true,
+          text: "My Data"
+        }
+      },
+  }),
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.chart-wrapper {
+  width: 800px;
+  height: 500px;
 }
 </style>
---
+++ src/components/LineChart.vue
@@ -0,0 +1,19 @@
+<script>
+import { Line } from 'vue-chartjs'
+
+export default {
+  extends: Line,
+  props: ['chartdata', 'options'],
+  data() {
+    return {
+    }
+  },
+  mounted () {
+    this.renderChart(this.chartdata, this.options)
+  }
+}
+</script>
+
+<style>
+
+</style>
